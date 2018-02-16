4a77800fb6eab1e3801076ba5f1336c9731c0fd8
gtarcea <glenn.tarcea@gmail.com>
2018-02-15 23:24:44 -0500

    First test demonstrating how yield would work

--- view_test.go
+++ view_test.go
@@ -9,17 +9,24 @@ import (
 	//. "github.com/golatte/latte"
 )
 
+var funcs = template.FuncMap{
+	"yield": func() (template.HTML, error) {
+		return template.HTML("<div>Hello</div>"), nil
+	},
+}
+
 var _ = Describe("View", func() {
 	Describe("Test layouts", func() {
 		It("Should find the layout", func() {
 			var b bytes.Buffer
-			t, err := template.New("layout.html").Parse("<layout>yield</layout>")
+			t, err := template.New("layout.html").Funcs(funcs).Parse("<layout>{{yield}}</layout>")
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(t).ShouldNot(BeNil())
 
 			err = t.Execute(&b, nil)
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(b.String()).Should(Equal("<layout>yield</layout>"))
+			//fmt.Println(b.String())
+			Expect(b.String()).Should(Equal("<layout><div>Hello</div></layout>"))
 		})
 	})
 
